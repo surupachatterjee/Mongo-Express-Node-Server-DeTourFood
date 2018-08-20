e1a4b78dcb90acc79596f9d9019d25411f4c2fdf
ShaivyT <shaivysh@gmail.com>
2018-08-20 04:37:46 -0400

    url server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:3000");
+        "https://detour-food.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
