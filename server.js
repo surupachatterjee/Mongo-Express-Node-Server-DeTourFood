94a3c26e6a61da9251a8326b126305128945be2f
Surupa Tushar Chatterjee <40750218+surupachatterjee@users.noreply.github.com>
2018-09-06 19:14:24 -0400

    CORS change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "https://detour-food.herokuapp.com");
+        "https://detour-food-stc.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
